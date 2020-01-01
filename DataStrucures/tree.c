bd447ae3d7bb189c723a8238f3cb51d9098768ea
Sayed Mohsen <48262280+SAmohsen@users.noreply.github.com>
2020-01-01 10:37:46 +0200

    tree

--- DataStrucures/tree.c
+++ DataStrucures/tree.c
@@ -1,17 +1,74 @@
-/*
- * tree.c
- *
- *  Created on: Dec 4, 2019
- *      Author: sayed
- */
-
-
 #include "tree.h"
-struct treenode {
-	char name, father;
-	int order;
-};
 
+static int stackArr[STACKSIZE];
+static int top = -1;
+static int queue[QUEUESIZE];
+static int rear = -1;
+static int front = -1;
+
+/* stack impelementation*/
+static int fullstack() {
+	if (top == (STACKSIZE - 1)) {
+		return 1;
+	}
+	return 0;
+}
+
+static int emptystack() {
+	if (top == -1) {
+		return 1;
+	}
+	return 0;
+}
+
+static void push(int data) {
+	if (!fullstack()) {
+		top++;
+		stackArr[top] = data;
+	}
+}
+
+static int pop() {
+	int data;
+	if (!emptystack()) {
+		data = stackArr[top];
+		top--;
+		return data;
+	}
+	return 0;
+}
+
+static int fullqueue() {
+	if (((rear + 1) % QUEUESIZE) == front) {
+		return 1;
+	} else
+		return 0;
+}
+
+static int emptyqueue() {
+
+	if (rear == front) {
+		return 1;
+	} else
+		return 0;
+
+}
+
+static void enqueue(int data) {
+	if (!fullqueue()) {
+		rear = (rear + 1) % QUEUESIZE;
+		queue[rear] = data;
+	}
+}
+
+static int dequeue() {
+	if (!emptyqueue()) {
+		front = (front + 1) % QUEUESIZE;
+		int data = queue[front];
+		return data;
+	}
+	return -1;
+}
 /*breadth first search : visit node in levels  */
 void breadthFristSearch(struct treenode *treeArray, int size) {
 
@@ -57,7 +114,6 @@ void depthFristSearch(struct treenode *treeArray, int size) {
 			break;
 		}
 	}
-
 	char parent;
 	char sons[size];/* worst case that rot has all sons */
 	/*traversal until stack is empty */
@@ -80,21 +136,20 @@ void depthFristSearch(struct treenode *treeArray, int size) {
 
 }
 
-
-
 /*driver code */
 
-/*
-int main() {
+int treedriver() {
 
-	struct treenode treeArray[12] = { { 'A', 'M', 2 }, { 'B', 'M', 1 }, { 'C',
-			'A', 1 }, { 'D', 'H', 3 }, { 'F', 'H', 1 }, { 'G', 'H', 2 }, { 'H',
-			'M', 3 }, { 'K', 'B', 2 }, { 'M', '0', 0 }, { 'P', 'K', 2 }, { 'X',
-			'K', 1 }, { 'Y', 'B', 1 } };
+	struct treenode treeArray[12] = {
+			{ 'A', 'M', 2 }, { 'B', 'M', 1 }, { 'C','A', 1 },
+			{ 'D', 'H', 3 }, { 'F', 'H', 1 }, { 'G', 'H', 2 },
+			{ 'H','M', 3 }, { 'K', 'B', 2 }, { 'M', '0', 0 },
+			{ 'P', 'K', 2 }, { 'X','K', 1 }, { 'Y', 'B', 1 } };
 
 	breadthFristSearch(treeArray, 12);
+	printf("\n");
 	depthFristSearch(treeArray, 12);
 
 	return 0;
 }
-*/
+
--- DataStrucures/tree.h
+++ DataStrucures/tree.h
@@ -1,29 +1,18 @@
-/*
- * tree.h
- *
- *  Created on: Dec 4, 2019
- *      Author: sayed
- */
 
 #ifndef DATASTRUCURES_TREE_H_
 #define DATASTRUCURES_TREE_H_
 
-
-/*
- * tree.c
- *
- *  Created on: Dec 4, 2019
- *      Author: sayed
- */
-
-
 #include <stdio.h>
-#include "queue.h"
-#include "stack.h"
+#define STACKSIZE 10
+#define QUEUESIZE 10
+struct treenode {
+	char name, father;
+	int order;
+};
 
-extern struct treenode ;
 /*breadth first search : visit node in levels  */
 void breadthFristSearch(struct treenode *treeArray, int size) ;
 /*depth first search : visit depth of nodes   */
 void depthFristSearch(struct treenode *treeArray, int size) ;
+int treedriver() ;
 #endif /* DATASTRUCURES_TREE_H_ */
