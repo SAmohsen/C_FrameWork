cd0739c2016d9180d36ac671ebda1c750959c195
Sayed Mohsen <48262280+SAmohsen@users.noreply.github.com>
2020-01-01 18:53:47 +0200

    interface

---
+++ c_FrameWork.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : c_FrameWork.c
+ Author      : sayed mohsen
+ Version     :
+ Copyright   : Your copyright notice
+ Description :Problem solving in C, Ansi-style
+ ============================================================================
+ */
+
+#include "c_FrameWork.h"
+
+/*solve your problems here */
+int main(void) {
+
+	return EXIT_SUCCESS;
+}
---
+++ c_FrameWork.h
@@ -0,0 +1,23 @@
+/*
+ * c_FrameWork.h
+ *
+ *  Created on: Jan 1, 2020
+ *      Author: sayed
+ */
+
+#ifndef C_FRAMEWORK_H_
+#define C_FRAMEWORK_H_
+
+#include <stdio.h>
+#include <math.h>
+#include<stdlib.h>
+#include "CFRAME/solving.h"
+#include "CFRAME/Algortims/sorting.h"
+#include "CFRAME/Algortims/searching.h"
+#include "CFRAME/DataStrucures/Hashing.h"
+#include "CFRAME/DataStrucures/queue.h"
+#include "CFRAME/DataStrucures/stack.h"
+#include "CFRAME/DataStrucures/tree.h"
+#include "CFRAME/DataStrucures/linkedlist.h"
+
+#endif /* C_FRAMEWORK_H_ */
